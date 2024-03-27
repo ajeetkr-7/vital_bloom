0e195baef03702e75963049a867263b97b68baf2
akki01133 <ajeetkumar83589@gmail.com>
2024-03-27 21:12:05 +0530

    added health package

--- lib/utils/permission_utils.dart
+++ lib/utils/permission_utils.dart
@@ -1,30 +1,26 @@
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionUtils {
-  /// Checks the current status of camera and microphone permissions.
-  /// Returns `true` if both camera and microphone permissions are granted.
-  Future<bool> getCameraAndMicrophonePermissionStatus() async {
-    // Get the current status of camera and microphone permissions
-    PermissionStatus cameraStatus = await Permission.camera.status;
-    PermissionStatus microphoneStatus = await Permission.microphone.status;
-
-    // Check if both camera and microphone permissions are granted
-    if (cameraStatus.isGranted && microphoneStatus.isGranted) {
+  static Future<bool> getactivityRecognitionStatus() async {
+    PermissionStatus activityRecognitionStatus =
+        await Permission.activityRecognition.status;
+    // PermissionStatus microphoneStatus = await Permission.microphone.status;
+    if (activityRecognitionStatus.isGranted) {
       return true;
     }
     return false;
   }
 
-  /// Requests camera and microphone permissions and returns `true` if granted.
-  Future<bool> askForPermission() async {
+  /// Requests permissions and returns `true` if granted.
+  static Future<bool> askForPermission() async {
     // Request camera and microphone permissions
     Map<Permission, PermissionStatus> status = await [
-      Permission.camera,
-      Permission.microphone,
+      Permission.activityRecognition,
+      // Permission.microphone,
     ].request();
 
-    // Check if both camera and microphone permissions are granted
-    if (status[Permission.camera]!.isGranted && status[Permission.microphone]!.isGranted) {
+    // Check if permissions are granted
+    if (status[Permission.activityRecognition]!.isGranted) {
       return true;
     }
     return false;
